ee1bdda43629e4b133969fa8aeac2b90d59cab1c
maca-dm <nadieimportante44@gmail.com>
2026-04-20 22:51:14 -0400

    Update Grafico2.py

--- Codigos/MacarenadelHoyo/Grafico2.py
+++ Codigos/MacarenadelHoyo/Grafico2.py
@@ -1,11 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import requests
+import io
+
+user = "Itz-oji"
+repo = "Tarea1_Visualizacion_Datos"
+branch = "main"
+folder_path = "Datos/MacarenadelHoyo"
+
+url_calificaciones = f"https://raw.githubusercontent.com/{user}/{repo}/{branch}/{folder_path}/Calificaciones.xlsx"
 
 try:
-    df = pd.read_excel('Calificaciones.xlsx')
+    response = requests.get(url_calificaciones)
+    if response.status_code == 200:
+        df = pd.read_excel(io.BytesIO(response.content))
+    else:
+        print(f"Error: No se encontró el archivo. Código de estado: {response.status_code}")
 except Exception as e:
-    print(f"Error al leer el archivo: {e}")
-    exit()
+    print(f"Ocurrió un error al intentar leer el Excel: {e}")
 
 df = df.iloc[:31]
 
